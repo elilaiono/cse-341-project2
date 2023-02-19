d4e25aa2186b789be2f4ab2093a33518069b79df
elilaiono <elilaiono@gmail.com>
2023-02-18 21:22:33 -0700

    added auth

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 
 const express = require('express');
 const router = express.Router();
+// const {  requiresAuth } = require('express-openid-connect');
 
 router.use('/Disc_Golf', require('./players'));
 router.use('/', require('./swagger'));
+
+// router.get('/', function (req, res) {
+//     res.render('index', {
+//         title: 'Auth0 Nodejs',
+//         isAuthenticated: req.oidc.isAuthenticated(),
+//     });
+// });
+
+// router.get('/profile', requiresAuth(), function (req, res) {
+//     res.render('profile', {
+//         userProfile: JSON.stringify(req.oidc.user, null, 2),
+//         title: 'Profile page'
+//     });
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const dotenv = require('dotenv');
+const logger = require('morgan');
+const path = require('path');
+const router = require('./routes/index');
+const { auth, requiresAuth } = require('express-openid-connect');
+
+dotenv.config();
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -25,3 +32,47 @@ mongodb.initDb((err) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
+
+// authentication
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+  config.baseURL = `http://localhost:${port}`;
+}
+
+
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
+// Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+  res.locals.user = req.oidc.user;
+  next();
+});
+
+app.use('/', router);
+
